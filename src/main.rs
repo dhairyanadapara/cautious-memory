75dcfa42b3707005552a23635479da4f8da4e9b4
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2023-11-02 23:33:57 +0530

    db connection

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 #[macro_use]
 extern crate rocket;
-
+use anyhow::Result;
+use dotenv::dotenv;
 use rocket::response::status::NoContent;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+use std::env;
 
 #[get("/health_check")]
-fn index() -> NoContent {
+fn health_check() -> NoContent {
     NoContent
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")?;
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+
+    rocket::build()
+        .mount("/", routes![health_check])
+        .manage(pool)
+        .launch()
+        .await?;
+
+    Ok(())
 }
